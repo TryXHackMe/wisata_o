dd39383430d728ad45f94d6401fc1fd43a16e127
TryXHackMe <onokosy@gmail.com>
2025-07-03 15:43:58 +0700

    Lanjutan

--- resources/views/home2.blade.php
+++ resources/views/home2.blade.php
@@ -3,6 +3,7 @@
         <div class="container mx-auto px-4 flex flex-col md:flex-row items-center">
             <div class="md:mb-0 mb-10 w-1/2">
                 <h1 class="text-4xl md:text-5xl">Jelajahi keindahan <span class="text-gradient">Indonesia</span></h1>
+                <p class="text-gray-600 mb-8 text-lg">Temukan pengalaman wisata tak terlupakan di berbagai destinasi eksotis di seluruh Nusantara</p>
             </div>
         </div>
     </section>
